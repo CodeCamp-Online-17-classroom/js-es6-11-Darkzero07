1c63f93289bab309519297163e0565ea4b9121cb
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-28 21:17:29 +0700

    js lab 155

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-// write code here
+// ผลลัพธ์ในบรรทัดที่มี * มีค่าเป็นอะไรและเพราะอะไร
+
+let [first, second, third] = ['Maya', 'Marisa', 'Chi'];
+console.log(first); // Maya
+console.log(second); // Marisa
+console.log(third); // Chi
+
+//เป็นการใช้ array destructuring ซึ่งอ้างอิงค่าตาม index ใน array
